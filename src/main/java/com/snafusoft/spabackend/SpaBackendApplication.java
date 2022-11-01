313e68a9a63a127a18b89e7c3c6ab839d57730fc
Michael Williams <michael.williams@orangebees.com>
2022-11-01 15:11:35 -0400

    working version with docker-compose

--- src/main/java/com/snafusoft/spabackend/SpaBackendApplication.java
+++ src/main/java/com/snafusoft/spabackend/SpaBackendApplication.java
@@ -1,18 +1,28 @@
 package com.snafusoft.spabackend;
 
 import com.snafusoft.spabackend.api.SpaApi;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.retry.annotation.EnableRetry;
 import org.springframework.scheduling.annotation.EnableAsync;
 
+@Slf4j
 @EnableAsync
 @EnableRetry
 @SpringBootApplication
 public class SpaBackendApplication {
 
+	@Value("${spring.datasource.username}")
+	private static String username;
+	@Value("${spring.datasource.url}")
+	private static String url;
+
 	public static void main(String[] args) {
+		log.info("mysql username: {}" ,username);
+		log.info("mysql connection string: {}" ,url);
 		var app = new SpringApplication(SpaBackendApplication.class);
 		app.run(args);
 	}
--- src/main/java/com/snafusoft/spabackend/coordinator/SpaBackendCoordinator.java
+++ src/main/java/com/snafusoft/spabackend/coordinator/SpaBackendCoordinator.java
@@ -1,5 +1,7 @@
 package com.snafusoft.spabackend.coordinator;
 
+
+
 import com.snafusoft.spabackend.model.NewsletterEmailItem;
 import com.snafusoft.spabackend.model.NewsletterSignupRequest;
 import com.snafusoft.spabackend.model.RegistrationRequest;
